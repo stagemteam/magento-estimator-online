233fa361490cae7137d5a389bd29134bd86966c3
Vlad Kozak <vlad.gem.typ@gmail.com>
2020-06-27 16:06:22 +0300

    Add base module files

--- app/code/community/Stagem/Estimator/Helper/Data.php
+++ app/code/community/Stagem/Estimator/Helper/Data.php
@@ -3,7 +3,7 @@
 /**
  * @author Stagem Team
  * @copyright Copyright (c) 2020 Stagem (https://www.stagem.com.ua)
- * @package Stagem_CustomerDate
+ * @package Stagem_Estimator
  */
 class Stagem_Estimator_Helper_Data extends Mage_Core_Helper_Abstract
 {
