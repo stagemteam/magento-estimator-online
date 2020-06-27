46fb67a7aa556a96207b314031e23012d0ed8992
Serhii Popov <1991183+popovserhii@users.noreply.github.com>
2020-06-27 15:33:12 +0300

    Create Data.php

---
+++ app/code/community/Stagem/CustomerDate/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @author Stagem Team
+ * @copyright Copyright (c) 2020 Stagem (https://www.stagem.com.ua)
+ * @package Stagem_CustomerDate
+ */
+class Stagem_Estimator_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
